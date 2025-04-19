5eb6620082cba7f8d319c0342d5889c2d5571fd5
ankita.uttam <ankita.uttam@adevinta.com>
2025-04-19 19:03:33 +0200

    Basic mapping between two simple same classes

---
+++ src/main/java/com/example/mapstruct/PojoMapping/PlainAndSimple/Destination.java
@@ -0,0 +1,11 @@
+package com.example.mapstruct.PojoMapping.PlainAndSimple;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Destination {
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/example/mapstruct/PojoMapping/PlainAndSimple/Source.java
@@ -0,0 +1,11 @@
+package com.example.mapstruct.PojoMapping.PlainAndSimple;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Source {
+    private String name;
+    private String description;
+}
